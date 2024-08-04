40ce195e20d6d2003279db498ef53183f881b2ad
unknown <adham111mahfoud@gmail.com>
2024-08-04 19:23:35 +0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
+import { Tajawal } from "next/font/google";
 import "./globals.css";
+import Navbar from "./(components)/Navbar/Navbar";
+import Footer from "./(components)/Footer/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Tajawal({ subsets: ["arabic"], weight: ["200", "300", "500", "700", "800", "900"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }
